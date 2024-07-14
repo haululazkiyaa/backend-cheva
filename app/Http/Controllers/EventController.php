25af032af571d84ca38ea819af21963bdc02dc09
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-07-14 15:57:28 +0700

    fix update events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -86,13 +86,39 @@ class EventController extends Controller
         }
 
         $validator = Validator::make($request->all(), [
-            // validasi lainnya
+            'event_name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category' => 'required|string|max:20',
+            'start_date' => 'required|date',
+            'finish_date' => 'required|date',
+            'start_time' => 'required|date_format:H:i:s',
+            'finish_time' => 'required|date_format:H:i:s',
+            'location' => 'required|string|max:30',
+            'contact_person' => 'required|string|max:15',
+            'registration_link' => 'required|string|max:255',
+            'status' => 'required|in:oncoming,ongoing,finished',
+            'user_id' => 'required|exists:users,user_id',
             'poster_file' => 'image|max:2048', // validasi untuk file gambar
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
+
+        $updatedData = [
+            'event_name' => $request->event_name,
+            'description' => $request->description,
+            'category' => $request->category,
+            'start_date' => $request->start_date,
+            'finish_date' => $request->finish_date,
+            'start_time' => $request->start_time,
+            'finish_time' => $request->finish_time,
+            'location' => $request->location,
+            'contact_person' => $request->contact_person,
+            'registration_link' => $request->registration_link,
+            'status' => $request->status,
+            'user_id' => $request->user_id,
+        ];
 
         if ($request->hasFile('poster_file')) {
             // menghapus file gambar lama dari storage
@@ -101,11 +127,11 @@ class EventController extends Controller
             // menyimpan file gambar baru ke storage dan mendapatkan path-nya
             $poster_file_path = $request->file('poster_file')->store('posters', 'public');
 
-            $event->poster_file_path = $poster_file_path;
+            $updatedData['poster_file_path'] = $poster_file_path;
         }
 
         // update atribut lainnya
-        $event->save();
+        $event->update($updatedData);
 
         return new EventResource($event);
     }
